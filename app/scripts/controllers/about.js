ee051c706ddaff7b07228fadefb0c87f838d17ad
antpowell <powell.anthony.e@gmail.com>
2015-12-28 20:27:42 -0600

    Blacks in Technology

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blacksApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the blacksApp
+ */
+angular.module('blacksApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blacksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blacksApp
+ */
+angular.module('blacksApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
