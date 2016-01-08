8bbd2ccf14ec476f556644a01bbdad8fc8598211
antpowell <powell.anthony.e@gmail.com>
2016-01-08 14:25:58 -0600

    End of workday Fri: added Podcast view, helper style, cosmetic clean up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/podcast', {
+        templateUrl: 'views/podcast.html',
+        controller: 'PodcastCtrl',
+        controllerAs: 'podcast'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/podcast.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blacksApp.controller:PodcastCtrl
+ * @description
+ * # PodcastCtrl
+ * Controller of the blacksApp
+ */
+angular.module('blacksApp')
+  .controller('PodcastCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
